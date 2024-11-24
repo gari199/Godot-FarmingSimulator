8a6302eb12908cdf6f6b414590dbc1f11d0aec45
gari199 <luiscobovazquez@hotmail.com>
2024-11-24 22:20:56 +0100

    ToolBar:style and tools + select tools

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -5,3 +5,12 @@ var player_direction: Vector2
 
 #Comes from Datatypes class and sets "None" as default tool.
 @export var current_tool: DataTypes.Tools = DataTypes.Tools.None
+@onready var hit_component: HitComponent = $HitComponent
+
+
+func _ready() -> void:
+	ToolManager.tool_selected.connect(equip_tool)
+
+func equip_tool(tool: DataTypes.Tools) -> void:
+	current_tool = tool
+	hit_component.current_tool = tool 
