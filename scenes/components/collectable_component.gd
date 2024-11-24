ecbe278b9d4a4f81505fb4219d3cb379dcdb48b8
gari199 <luiscobovazquez@hotmail.com>
2024-11-24 23:31:13 +0100

    Collectables added (easy)

--- scenes/components/collectable_component.gd
+++ scenes/components/collectable_component.gd
@@ -6,7 +6,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		print("Collected")
+		print("Collected ", collectable_name)
 		# We get the parent as the component will be attached to different items (log, fruits..)
 		# and those are the ones who have to dissapear
 		get_parent().queue_free()
